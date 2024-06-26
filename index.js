2ba4944a4acd28e3d2def9b5299f3ee3546a84b9
Monalisa Onyango <+>
2024-06-26 16:37:32 +0300

    ph1 function lab continue

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+//defines saturdayFun function declaration as specified
+function saturdayFun(defaultActivity='roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`;
+};
+console.log(saturdayFun());
+
+
+//defines mondayWork function expression as specified
+function mondayWork(defaultActivity1='go to the office'){
+    return `This Monday, I will ${defaultActivity1}.`;
+};
+console.log(mondayWork());
+
+
+function wrapAdjective( wraps='*'){
+    return function (emphatic='a hard worker'){ 
+        return `You are ${wraps}${emphatic}${wraps}!`
+    }
+}
+
+
+
+
+
+
+
